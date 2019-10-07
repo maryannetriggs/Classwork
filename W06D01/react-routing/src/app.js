a9ce658363b299a2b1f94f07cf018abb120a463f
Mary-Anne Triggs <maryannetriggs@gmail.com>
2019-10-07 13:58:56 +0100

    cw

--- W06D01/react-routing/src/app.js
+++ W06D01/react-routing/src/app.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+
+import Home from './components/Home'
+import Index from './components/Index'
+import Show from './components/Show'
 
 class App extends React.Component {
   render() {
-    return <h1>Hello World</h1>
+    return (
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Home Page</Link>
+          <Link to="/index">Index Page</Link>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/index/:id/" component={Show} />
+          <Route path="/index" component={Index} />
+        </Switch>
+      </BrowserRouter>
+    )
   }
 }
-
+    
 ReactDOM.render(
   <App />,
   document.getElementById('root')
---
+++ W06D01/react-routing/src/components/Show.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Show = () => <h1>Show Page</h1>
+
+export default Show
