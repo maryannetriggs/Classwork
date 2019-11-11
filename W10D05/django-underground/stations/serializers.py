bd7240e924a43134ffdbcb121c4de8572cacfb06
Mary-Anne Triggs <maryannetriggs@gmail.com>
2019-11-11 11:20:18 +0000

    upating cw

--- W10D05/django-underground/stations/serializers.py
+++ W10D05/django-underground/stations/serializers.py
@@ -45,6 +45,17 @@ class StationSerializer(serializers.ModelSerializer):
     zone = NestedZoneSerializer() # shows zone id and zone number
     lines = NestedLineSerializer(many=True) # shows line id and line name
 
+    def create(self, data): # data is the incoming json converted to a python dict
+        zone_data = data.pop('zone') # stored and removing from the data object
+        lines_data = data.pop('lines')
+
+        station = Station(**data) # create the station from the data leftover
+        station.zone = Zone.objects.get(**zone_data) # add the zones back in after finding the correct one from the db
+        lines = [Line.objects.get(**line_data) for line_data in lines_data] # finding all the lines that need to be added
+        station.save() # we need to save to ensure primary key is beign created before attempting to set a many-many relationship on the model
+        station.lines.set(lines) # now we can set those lines
+        return station # returning the newly created station to be sent in the response to the client
+
     class Meta:
         model = Station
         fields = ('id', 'name', 'lat', 'lon', 'is_night_tube', 'zone', 'lines')
